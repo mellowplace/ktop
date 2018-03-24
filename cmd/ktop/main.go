abc27b22a044bd3f67e6adcef4297ed9e9f9c065
mellowplace <rob@mellowplace.com>
2018-03-24 13:02:05 +0000

    License and connecting to k8s

--- cmd/ktop/main.go
+++ cmd/ktop/main.go
@@ -3,12 +3,50 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 func main() {
 	kubeContext := flag.String("context", "", "kubectl context name, empty will use the current")
+	var kubeConfig *string
+	if home := homeDir(); home != "" {
+		kubeConfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeConfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
 
 	flag.Parse()
 
 	fmt.Println(*kubeContext)
+
+	// use the current context in kubeconfig
+
+	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: *kubeConfig},
+		&clientcmd.ConfigOverrides{
+			ClusterInfo:    clientcmdapi.Cluster{Server: ""},
+			CurrentContext: *kubeContext,
+		}).ClientConfig()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	_, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
